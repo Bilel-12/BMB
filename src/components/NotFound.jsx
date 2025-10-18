c099fbc3e6eae6f6b1107f7aa6dbce9e9717e0e7
bd_woujoud <wwwwboudhina@gmail.com>
2025-10-18 15:31:42 +0100

    404

--- src/components/NotFound.jsx
+++ src/components/NotFound.jsx
@@ -1,8 +1,13 @@
+import { Link } from "react-router-dom";
 
 function NotFound() {
     return (
-        <div className='d-flex align-items-center justify-content-center fw-bold text-5xl'>404</div>
-    )
+        <div className="d-flex flex-column align-items-center justify-content-center min-vh-100 text-center">
+            <h1 className="fw-bold text-5xl mb-4">404</h1>
+            <p className="text-lg mb-4">الصفحة التي تبحث عنها غير موجودة.</p>
+            <Link to="/" className="btn btn-primary">العودة للرئيسية</Link>
+        </div>
+    );
 }
 
-export default NotFound
+export default NotFound;
